e841d14604516a9f0f9579bd8b6e14f68b06b0e2
warren pitterson <warrenpitterson@gmail.com>
2019-07-02 16:55:17 +0100

    Added .AddJsonOptions method

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -19,3 +19,4 @@ namespace CityInfo.API.Models
 
     }
 }
+ 
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -16,7 +17,17 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                //Makes Property Titles start with upper case
+            .AddJsonOptions(o => {
+                 if (o.SerializerSettings.ContractResolver != null)
+                 {
+                     var castedResolver = o.SerializerSettings.ContractResolver
+                     as DefaultContractResolver;
+                     castedResolver.NamingStrategy = null;
+                 }
+
+             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
